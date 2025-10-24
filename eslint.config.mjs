57595a2034439d78476107a0900e666925a1f391
Gustavo Viegas <gustavo@garten.com.br>
2025-10-24 18:01:11 -0300

    feat: estlint, prettier e .vscode

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,28 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+	{
+		ignores: [
+			'.nuxt',
+			'.husky',
+			'.output',
+			'.vscode',
+			'build',
+			'public',
+			'node_modules',
+		],
+	},
+	{
+		rules: {
+			'no-console': 1,
+			'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+			eqeqeq: ['error', 'always'],
+			'vue/valid-v-bind': 'error',
+			'vue/no-multiple-template-root': 'error',
+			'prefer-const': 'error',
+			'vue/require-default-prop': 'off',
+			'vue/multi-word-component-names': 'off',
+			'no-undef': 'off',
+		},
+	},
 )
